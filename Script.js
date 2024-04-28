33f811f34152df3f4b33477a25c37c55fc163252
raghav69-crypt <raghavgupta241@gmail.com>
2024-04-28 20:29:05 +0530

    Updating Changes to Styling and Scripting

--- Script.js
+++ Script.js
@@ -7,10 +7,36 @@ const options = {
 	}
 };
 
-fetch("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi" , options)
+const weatherfind = (city)=>{
+	// city will map with cityName in html
+cityName.innerHTML = city; 
+fetch("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city , options)
 .then(response => response.json())
-.then(response => console.log(response))
-.catch(err => console.log(err));
+.then((response) => {
+    console.log(response);
+
+    document.getElementById("cloud_pct").innerHTML = response.cloud_pct;
+    document.getElementById("feels_like").innerHTML = response.feels_like;
+    document.getElementById("humidity").innerHTML = response.humidity;
+    document.getElementById("max_temp").innerHTML = response.max_temp;
+    document.getElementById("min_temp").innerHTML = response.min_temp;
+    document.getElementById("sunrise").innerHTML = response.sunrise;
+    document.getElementById("sunset").innerHTML = response.sunset;
+    document.getElementById("temp").innerHTML = response.temp;
+    // document.getElementById("wind_degrees").innerHTML = response.wind_degrees;
+    document.getElementById("wind_speed").innerHTML = response.wind_speed;
+})
+.catch(err => console.error(err));
+}
+// Button based eventlistner , check sumbit id in html
+submit.addEventListener("click",(e)=>{
+	// Prevent Reloading : preventdefault
+	e.preventDefault() 
+	weatherfind(city.value)
+})
+
+
+
 // try {
 // 	const response = await fetch(url, options);
 // 	const result = await response.text();
